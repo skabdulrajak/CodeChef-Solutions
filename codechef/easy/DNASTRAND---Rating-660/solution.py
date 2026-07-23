0858c44b6808251feae709fd2f2b6d6a73989c2c
skabdulrajak <rajaksk319@gmail.com>
2026-07-23 11:44:00 +0530

    ✅ DNASTRAND - Rating 660 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-23

--- codechef/easy/DNASTRAND---Rating-660/solution.py
+++ codechef/easy/DNASTRAND---Rating-660/solution.py
@@ -1,20 +1,11 @@
 # cook your dish here
 t=int (input())
 while t:
-    n=int (input())
-    s=input()
-    s = s.replace("A", "x")
-    s = s.replace("T", "y")
-    s = s.replace("C", "z")
-    s = s.replace("G", "w")
-    
-    s = s.replace("x", "T")
-    s = s.replace("y", "A")
-    s = s.replace("z", "G")
-    s = s.replace("w", "C")
-    
-   
-
-  
-    print (s)
-    t=t-1
+   n=int (input())
+   s=input()
+   result=[]
+   mapping ={'A':'T','T':'A','C':'G','G':'C'}
+   for char in s:
+       result.append(mapping[char])
+   print ("".join(result))
+   t=t-1
