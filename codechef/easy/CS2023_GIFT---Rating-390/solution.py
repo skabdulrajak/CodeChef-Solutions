9c1403017dbd9d286ea37ae8968d00754fccd405
skabdulrajak <rajaksk319@gmail.com>
2026-07-24 23:01:02 +0530

    ✅ CS2023_GIFT - Rating 390 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-24

--- codechef/easy/CS2023_GIFT---Rating-390/solution.py
+++ codechef/easy/CS2023_GIFT---Rating-390/solution.py
@@ -1,11 +1,6 @@
 # cook your dish here
-t=int (input())
-while t:
-    n=int (input())
-    if n<4:
-        print ("MILD")
-    elif n>=4 and n<7:
-        print ("MEDIUM")
-    else:
-        print ("HOT")
-    t=t-1
+x,n,m=map(int,input().split())
+if x+m >= n:
+    print ("YES")
+else:
+    print ("NO")
