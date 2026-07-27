0f3ee8a8b6c1787742214ececd5a6f2f1308c80c
skabdulrajak <rajaksk319@gmail.com>
2026-07-27 21:38:20 +0530

    ✅ VACDI | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-27

--- codechef/medium/VACDI/solution.py
+++ codechef/medium/VACDI/solution.py
@@ -3,7 +3,7 @@ n=int (input())
 arr=list (map(int,input().split()))
 vas=list (map(int,input().split()))
 count=0
-for i in range (1,n):
+for i in range (n):
     if arr[i]<vas[i]:
         count+=1
 if count ==n:
