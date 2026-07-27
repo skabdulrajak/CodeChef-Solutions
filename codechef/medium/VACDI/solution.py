ea75d9ea39950ccc1a493287cd76bb754e094d2b
skabdulrajak <rajaksk319@gmail.com>
2026-07-27 21:39:26 +0530

    ✅ VACDI | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-27

--- codechef/medium/VACDI/solution.py
+++ codechef/medium/VACDI/solution.py
@@ -2,11 +2,11 @@
 n=int (input())
 arr=list (map(int,input().split()))
 vas=list (map(int,input().split()))
-count=0
+arr.sort()
+vas.sort()
 for i in range (n):
     if arr[i]<vas[i]:
-        count+=1
-if count ==n:
-    print ("YES")
+        print ("No")
+        break
 else:
-    print ("NO")
+    print ("YES")
