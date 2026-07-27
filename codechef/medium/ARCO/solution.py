b9afc03e25be011369e5e406e3fcd4193dfafc7f
skabdulrajak <rajaksk319@gmail.com>
2026-07-27 21:25:32 +0530

    ✅ ARCO | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-27

--- codechef/medium/ARCO/solution.py
+++ codechef/medium/ARCO/solution.py
@@ -1,10 +1,10 @@
 # cook your dish here
 n=int (input())
 arr=list (map(int,input().split()))
-s=[]
+s=arr
 for i in range (len(arr)):
-   for j in range (i+1,len(arr)):
-    if arr[i]!=arr[j]:
-        s.append(arr[i])
-        break
+  
+    if arr[i]==arr[i+1]:
+        s.remove(arr[i])
+        
 print (len(s))
